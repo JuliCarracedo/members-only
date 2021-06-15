d14f505b7860b5f555e8280c1ef9fdfcc064ebe5
Enio N. de Souza <enionsouza@gmail.com>
2021-06-15 14:40:38 -0400

    Add seeds for Users and Posts Tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+  {
+    username: 'user_001',
+    email: 'user_001@example.com',
+    password: 'secret',
+    password_confirmation: 'secret'
+  },
+  {
+    username: 'user_002',
+    email: 'user_002@example.com',
+    password: 'secret',
+    password_confirmation: 'secret'
+  },
+  {
+    username: 'user_003',
+    email: 'user_003@example.com',
+    password: 'secret',
+    password_confirmation: 'secret'
+  }
+])
+
+posts = Post.create([
+  {
+    title: 'A clever post!',
+    body: 'A lot of very interesting ideas to share',
+    user_id: 1
+  },
+  {
+    title: 'Another clever post!',
+    body: 'A lot of very interesting new ideas to share, yet again!',
+    user_id: 1
+  },
+  {
+    title: 'Interesting ideas about programming',
+    body: 'This is the best way to become a good programmer!',
+    user_id: 2
+  }
+])
