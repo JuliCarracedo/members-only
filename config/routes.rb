cfe067a58dc372d33c808d5ffb5f5bc88cee6b7c
Enio N. de Souza <enionsouza@gmail.com>
2021-06-15 14:41:23 -0400

    Fix webpacker error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  devise_for :users do
-    resources :posts
-  end
+  resources :posts
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
