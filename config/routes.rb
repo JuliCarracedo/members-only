bd7089c1dedd6a639de8c335d0d4c07babffacd1
Enio N. de Souza <enionsouza@gmail.com>
2021-06-16 18:29:56 -0400

    Add username field permissions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
