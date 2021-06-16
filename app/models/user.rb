9a23d048cc3297b0da46eefcb4e88f1e80aed02d
Enio N. de Souza <enionsouza@gmail.com>
2021-06-16 18:27:36 -0400

    Add validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, length: { in: 1..20 }, uniqueness: true
   validates :email, presence: true, format: { with: /\w+@\w+(.\w+)/ }, uniqueness: true
   validates :password, length: { in: 6..20 }, presence: true
   validate :password_requirements_are_met
 
   private
+
   def password_requirements_are_met
     rules = {
       ' must contain at least one lowercase letter' => /[a-z]+/,
@@ -18,7 +20,7 @@ class User < ApplicationRecord
     }
 
     rules.each do |message, regex|
-    errors.add(:password, message) unless password.match(regex)
+      errors.add(:password, message) unless password.match(regex)
     end
   end
 end
